ce806f98ac05ac23b5877d48f804a46d55b4119d
iyue <ys1231@126.com>
2024-08-02 18:47:53 +0800

    init

--- app/src/main/java/com/placeholder/placeholder/Main.java
+++ app/src/main/java/com/placeholder/placeholder/Main.java
@@ -1,4 +1,4 @@
-package com.jgwl.patchviber;
+package com.placeholder.placeholder;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -10,15 +10,15 @@ public class Main implements IXposedHookLoadPackage {
     public static final String TAG = "PatchViber";
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        if (! loadPackageParam.packageName.equals("com.viber.voip")){
+        if (! loadPackageParam.packageName.equals("placeholder package name")){
             return;
         }
         XposedBridge.log(TAG + ": start hook " + loadPackageParam.packageName);
         ClassLoader classLoader = loadPackageParam.classLoader;
-        XposedHelpers.findAndHookMethod("fa.i", classLoader, "g", new XC_MethodReplacement() {
+        XposedHelpers.findAndHookMethod("placeholder iyue", classLoader, "placeholder iyue", new XC_MethodReplacement() {
             @Override
             protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                XposedBridge.log(TAG + ": replace fa.i.g return false");
+                XposedBridge.log(TAG + ": replace placeholder return false");
                 return false;
             }
         });
